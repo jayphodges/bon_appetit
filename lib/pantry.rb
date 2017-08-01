caa2613580ffa3921d30380124edb9982aa8b119
jayphodges <jayphodges@gmail.com>
2017-08-01 09:49:55 -0600

    Add working iteration 2

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,4 +1,5 @@
 require 'pry'
+
 class Pantry
 
   attr_reader :stock
@@ -14,5 +15,27 @@ class Pantry
   def stock_check(item)
     @stock[item.to_s]
   end
-  
+
+  def convert_units(recipe)
+    result = Hash.new
+    recipe.ingredients.each_pair do |key, value|
+      result[key] = converter(value)
+    end
+    result
+  end
+
+  private
+
+  def converter(unit)
+    if unit < 1
+      {quantity: (unit * 1000).to_i,
+         units: "Milli-Units"}
+    elsif unit > 100
+      {quantity: (unit / 100).round(0),
+         units: "Centi-Units"}
+    else
+      {quantity: unit, units: "Universal Units"}
+    end
+  end
+
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -25,4 +25,19 @@ class PantryTest < Minitest::Test
     assert_equal 20, pantry.stock_check("Cheese")
   end
 
+  def test_converting_units
+    pantry = Pantry.new
+    centi = pantry.convert_units({:Cheese => 500})
+    milli = pantry.convert_units({:Pepper => 0.05})
+    uni = pantry.convert_units({:Meat => 65})
+    centi_units = {:Cheese=>{:quantity=>5, :units=>"Centi-Units"}}
+    milli_units = {:Pepper=>{:quantity=>50, :units=>"Milli-Units"}}
+    uni_units = {:Meat=>{:quantity=>65, :units=>"Universal Units"}}
+    assert_equal centi_units, centi
+    assert_equal milli_units, milli
+    assert_equal uni_units, uni
+
+  end
+
+
 end
