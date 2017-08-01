22500d1d8bb07f81056ef31bfc7b6c3e414b7d32
jayphodges <jayphodges@gmail.com>
2017-08-01 09:12:39 -0600

    Add initial structure for iteration 0

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,16 @@
 class Pantry
 
+  attr_reader :stock
+
+  def initialize
+    @stock = Hash.new(0)
+  end
+
+  def restock(item)
+
+  end
+
+  def stock_check
+
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,17 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def test_it_exists
+    pantry = Pantry.new
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_it_can_be_stocked
+
+end
+
+  def test_checking_stock
+
+  end
+
 end
