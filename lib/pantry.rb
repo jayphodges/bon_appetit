16ae40cebeeefd78ffa71a3da93ea3cafad320e1
jayphodges <jayphodges@gmail.com>
2017-08-01 11:18:54 -0600

    Add additional refactoring of iteration 3

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -75,25 +75,25 @@ end
 # pantry = Pantry.new
 # binding.pry
 #=====================================
-r1 = Recipe.new("Cheese Pizza")
-r1.add_ingredient("Cheese", 20)
-r1.add_ingredient("Flour", 20)
-
-r2 = Recipe.new("Brine Shot")
-r2.add_ingredient("Brine", 10)
-
-r3 = Recipe.new("Peanuts")
-r3.add_ingredient("Raw nuts", 10)
-r3.add_ingredient("Salt", 10)
-pantry = Pantry.new
-pantry.add_to_cookbook(r1)
-pantry.add_to_cookbook(r2)
-pantry.add_to_cookbook(r3)
-
-pantry.restock("Cheese", 10)
-pantry.restock("Flour", 20)
-pantry.restock("Brine", 40)
-pantry.restock("Raw nuts", 20)
-pantry.restock("Salt", 20)
-pantry.how_many_can_i_make
-binding.pry
+# r1 = Recipe.new("Cheese Pizza")
+# r1.add_ingredient("Cheese", 20)
+# r1.add_ingredient("Flour", 20)
+#
+# r2 = Recipe.new("Brine Shot")
+# r2.add_ingredient("Brine", 10)
+#
+# r3 = Recipe.new("Peanuts")
+# r3.add_ingredient("Raw nuts", 10)
+# r3.add_ingredient("Salt", 10)
+# pantry = Pantry.new
+# pantry.add_to_cookbook(r1)
+# pantry.add_to_cookbook(r2)
+# pantry.add_to_cookbook(r3)
+# #
+# pantry.restock("Cheese", 10)
+# pantry.restock("Flour", 20)
+# pantry.restock("Brine", 40)
+# pantry.restock("Raw nuts", 20)
+# pantry.restock("Salt", 20)
+# pantry.how_many_can_i_make
+# binding.pry
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -8,32 +8,32 @@ require 'mocha'
 
 class PantryTest < Minitest::Test
 
+  def setup
+    @pantry = Pantry.new
+  end
+
   def test_it_exists
-    pantry = Pantry.new
-    assert_instance_of Pantry, pantry
+    assert_instance_of Pantry, @pantry
   end
 
   def test_it_can_be_stocked
-    pantry = Pantry.new
-    pantry.restock("Cheese", 10)
-    assert_equal ({"Cheese"=>10}), pantry.stock
-    pantry.restock("Cheese", 10)
-    assert_equal ({"Cheese"=>20}), pantry.stock
+    @pantry.restock("Cheese", 10)
+    assert_equal ({"Cheese"=>10}), @pantry.stock
+    @pantry.restock("Cheese", 10)
+    assert_equal ({"Cheese"=>20}), @pantry.stock
   end
 
   def test_checking_stock
-    pantry = Pantry.new
-    pantry.restock("Cheese", 10)
-    assert_equal 10, pantry.stock_check("Cheese")
-    pantry.restock("Cheese", 10)
-    assert_equal 20, pantry.stock_check("Cheese")
+    @pantry.restock("Cheese", 10)
+    assert_equal 10, @pantry.stock_check("Cheese")
+    @pantry.restock("Cheese", 10)
+    assert_equal 20, @pantry.stock_check("Cheese")
   end
 
   def test_converting_units
-    pantry = Pantry.new
-    centi = pantry.converter(500)
-    milli = pantry.converter(0.05)
-    uni = pantry.converter(65)
+    centi = @pantry.converter(500)
+    milli = @pantry.converter(0.05)
+    uni = @pantry.converter(65)
     centi_units = {:quantity=>5, :units=>"Centi-Units"}
     milli_units = {:quantity=>50, :units=>"Milli-Units"}
     uni_units = {:quantity=>65, :units=>"Universal Units"}
@@ -43,25 +43,54 @@ class PantryTest < Minitest::Test
   end
 
   def test_adding_to_cookbook
-    pantry = Pantry.new
-    pantry.add_to_cookbook("recipe")
-    assert_equal "recipe", pantry.cookbook
+    @pantry.add_to_cookbook("recipe")
+    assert_equal ["recipe"], @pantry.cookbook
   end
 
   def test_what_can_i_make
-
+    @pantry.add_to_cookbook(recipe2)
+    fill_pantry
+    actual = @pantry.what_can_i_make
+    expected = "somehash"
+    binding.pry
+    assert_equal expected, actual
   end
 
   def test_how_many_can_i_make
 
   end
 
-  def recipe
-    r = Recipe.new("Spicy Cheese Pizza")
-    r.add_ingredient("Cayenne Pepper", 0.025)
-    r.add_ingredient("Cheese", 75)
-    r.add_ingredient("Flour", 500)
-    r
+  def recipe1
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+    r1
   end
+  #
+  def recipe2
+    r2 = Recipe.new("Brine Shot")
+    r2.add_ingredient("Brine", 10)
+    r2
+  end
+  #
+  # def recipe3
+  #   r3 = Recipe.new("Peanuts")
+  #   r3.add_ingredient("Raw nuts", 10)
+  #   r3.add_ingredient("Salt", 10)
+  #   r3
+  # end
+  #
+  #
+  def fill_pantry
+    @pantry.restock("Cheese", 10)
+    @pantry.restock("Flour", 20)
+    @pantry.restock("Brine", 40)
+    @pantry.restock("Raw nuts", 20)
+    @pantry.restock("Salt", 20)
+  end
+  #
+  # def fill_cookbook
+  #
+  # end
 
 end
